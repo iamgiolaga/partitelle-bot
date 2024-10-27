176d726cef37bd8a0161ec408f7eaaf5ae780de1
Giovanni Laganà <giovanni.lagana1995@gmail.com>
2024-10-27 23:39:18 +0100

    chore: rollbacked first connection statement

--- db/connection.py
+++ db/connection.py
@@ -8,6 +8,13 @@ def connect():
         conn = psycopg2.connect(**params)
         conn.autocommit = True
         cur = conn.cursor()
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
         return cur
     except (Exception, psycopg2.DatabaseError) as error:
         print("There was an issue during the db connection:")
